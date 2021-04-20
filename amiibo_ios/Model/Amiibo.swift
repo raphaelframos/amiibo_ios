da3c1a358640f5ae3c4fe054fe1256c242efa68a
Raphael Ramos <raphaelframos@gmail.com>
2021-04-20 00:28:02 -0300

    Amiibo repository com alamofire

---
+++ amiibo_ios/Model/Amiibo.swift
@@ -0,0 +1,21 @@
+//
+//  Amiibo.swift
+//  amiibo_ios
+//
+//  Created by Raphael Ferreira Ramos on 19/04/21.
+//
+
+import Foundation
+
+struct Amiibo {
+    
+    var amiiboSeries = ""
+    var character = ""
+    var gameSeries = ""
+    var head = ""
+    var image = ""
+    var name = ""
+    var tail = ""
+    var type = ""
+    
+}
