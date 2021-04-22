6fc81acd7388730e8cf5913129785c134afe7917
Raphael Ramos <raphaelframos@gmail.com>
2021-04-22 06:03:47 -0300

    Celula criada para o feed

---
+++ amiibo_ios/Cell/FeedTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedTableViewCell.swift
+//  amiibo_ios
+//
+//  Created by Raphael Ferreira Ramos on 22/04/21.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var imageViewAvatar: UIImageView!
+    @IBOutlet weak var labelName: UILabel!
+    
+}
